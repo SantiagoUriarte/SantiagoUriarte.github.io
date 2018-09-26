47909b373b9261a0dbfb33ecc21102e6a241764e
Santiago Uriarte <c.SantiagoU@gmail.com>
2018-09-26 04:52:55 -0700

    Added modals
    Added pop up modals for extra clarity.
    
    cleaned up email code

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 function copyEmail() {
     var email = document.getElementById("email");
+    email.style.display="block";
     email.select();
     document.execCommand("copy");
-    console.log(email.value);
+    email.style.display="none";
     alert("email copied to clipboard");
+}
+
+function activateModal(element_id) {
+    var modal_id = element_id + "modal"
+    document.getElementById(modal_id).classList.add("is-active")
+}
+
+function closeModal(element) {
+    modal_id = element.parentElement.id;
+    document.getElementById(modal_id).classList.remove("is-active")
 }
 
 $(document).ready(function () {
@@ -19,7 +30,7 @@ $(document).ready(function () {
             var hash = this.hash;
 
             // Using jQuery's animate() method to add smooth page scroll
-            // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+            // The optional number specifies the number of milliseconds it takes to scroll to the specified area
             $('html, body').animate({
                 scrollTop: $(hash).offset().top
             }, 1400, function () {
